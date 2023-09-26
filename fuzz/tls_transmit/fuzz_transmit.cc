0d35ab1e1dfc677b66388503cf0b241c3d5e2725
Pengyu Chen <chenpengyu_1024@163.com>
2023-09-26 13:05:23 +0000

    fuzz/*: fuzz rats-tls APIs in host mode
    use libfuzzer to fuzz rats-tls API `rats_tls_init()`,
    ,`rats_tls_transmit()` and
     `rats_tls_receive()` in host mode. fuzz for rats_tls_negotiate()
    has some bugs, it would be fixed and committed later.
    
    Signed-off-by: Pengyu Chen <chenpengyu_1024@163.com>

---
+++ fuzz/tls_transmit/fuzz_transmit.cc
@@ -0,0 +1,182 @@
+/* Copyright (c) 2021 Intel Corporation
+ * Copyright (c) 2020-2021 Alibaba Cloud
+ *
+ * SPDX-License-Identifier: Apache-2.1
+ */
+
+extern "C" {
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include "rats-tls/api.h"
+#include "rats-tls/log.h"
+#include "rats-tls/claim.h"
+}
+#include <fuzzer/FuzzedDataProvider.h>
+#define FUZZ_IP		   "127.0.0.1"
+#define FUZZ_PORT	   1234
+#define CUSTOM_CLAIMS_SIZE 10
+
+int user_callback(void *args)
+{
+	rtls_evidence_t *ev = (rtls_evidence_t *)args;
+
+	printf("verify_callback called, claims %p, claims_size %zu, args %p\n", ev->custom_claims,
+	       ev->custom_claims_length, args);
+	for (size_t i = 0; i < ev->custom_claims_length; ++i) {
+		printf("custom_claims[%zu] -> name: '%s' value_size: %zu value: '%.*s'\n", i,
+		       ev->custom_claims[i].name, ev->custom_claims[i].value_size,
+		       (int)ev->custom_claims[i].value_size, ev->custom_claims[i].value);
+	}
+	return 1;
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+	if (size < sizeof(rats_tls_conf_t) + 10 * sizeof(claim_t) + 50 * 10 + 100) {
+		return 0;
+	}
+	FuzzedDataProvider fuzzed_data(data + sizeof(rats_tls_conf_t),
+				       size - sizeof(rats_tls_conf_t));
+
+	char attester_types[10][25] = { "nullattester", "",	   "sgx_la",	"csv",
+					"sev",		"sev_snp", "tdx_ecdsa", "sgx_ecdsa" };
+	strcpy(attester_types[8], fuzzed_data.ConsumeBytesWithTerminator(20, '\0').data());
+	if (fuzzed_data.remaining_bytes() < 0) {
+		return 0;
+	}
+	for (int i = 0; i < 9; i++) {
+		char verifier_types[10][25] = { "nullverifier", "",	     "sgx_la",
+						"csv",		"sev",	     "sev_snp",
+						"tdx_ecdsa",	"tdx_ecdsa", "sgx_ecdsa_qve" };
+		strcpy(verifier_types[9], fuzzed_data.ConsumeBytesWithTerminator(20, '\0').data());
+		if (fuzzed_data.remaining_bytes() < 0) {
+			return 0;
+		}
+		for (int j = 0; j < 10; j++) {
+			char tls_types[4][25] = { "nulltls", "", "openssl" };
+			strcpy(tls_types[3],
+			       fuzzed_data.ConsumeBytesWithTerminator(20, '\0').data());
+			if (fuzzed_data.remaining_bytes() < 0) {
+				return 0;
+			}
+			for (int k = 0; k < 4; k++) {
+				char crypto_types[4][25] = { "nullcrypto", "", "openssl" };
+				strcpy(crypto_types[3],
+				       fuzzed_data.ConsumeBytesWithTerminator(20, '\0').data());
+				if (fuzzed_data.remaining_bytes() < 0) {
+					return 0;
+				}
+				for (int l = 0; l < 4; l++) {
+					rats_tls_conf_t conf;
+					memcpy(&conf,
+					       fuzzed_data.ConsumeBytes<uint8_t>(sizeof(conf))
+						       .data(),
+					       sizeof(conf));
+					if (fuzzed_data.remaining_bytes() < 0) {
+						return 0;
+					}
+					conf.log_level = RATS_TLS_LOG_LEVEL_INFO;
+					conf.cert_algo = RATS_TLS_CERT_ALGO_DEFAULT;
+					conf.api_version = 0;
+					conf.flags = fuzzed_data.ConsumeIntegral<long>();
+
+					strcpy(conf.attester_type, attester_types[i]);
+					strcpy(conf.verifier_type, verifier_types[j]);
+					strcpy(conf.tls_type, tls_types[k]);
+					strcpy(conf.crypto_type, crypto_types[l]);
+
+					int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+					if (sockfd < 0) {
+						RTLS_ERR("socked create failed \n");
+						continue;
+					}
+
+					claim_t custom_claims[CUSTOM_CLAIMS_SIZE];
+					std::vector<std::string> str_lists;
+					for (int c = 0; c < CUSTOM_CLAIMS_SIZE; c++) {
+						std::vector<char> vec_str =
+							fuzzed_data.ConsumeBytesWithTerminator(
+								50, '\0');
+						std::string str(vec_str.begin(), vec_str.end());
+						str_lists.push_back(str);
+						custom_claims[c].value =
+							(uint8_t *)str_lists[c].c_str();
+						custom_claims[c].value_size =
+							(strlen(str_lists[c].c_str()) + 1) *
+							sizeof(char);
+						if (fuzzed_data.remaining_bytes() <= 0) {
+							return 0;
+						}
+						custom_claims[c].name = "key";
+					}
+					conf.custom_claims = (claim_t *)custom_claims;
+					conf.custom_claims_length = CUSTOM_CLAIMS_SIZE;
+
+					struct sockaddr_in s_addr;
+					memset(&s_addr, 0, sizeof(s_addr));
+					s_addr.sin_family = AF_INET;
+					s_addr.sin_port = htons(FUZZ_PORT);
+
+					if (inet_pton(AF_INET, FUZZ_IP, &s_addr.sin_addr) != 1) {
+						continue;
+					}
+
+					if (connect(sockfd, (struct sockaddr *)&s_addr,
+						    sizeof(s_addr)) == -1) {
+						close(sockfd);
+						continue;
+					}
+
+					rats_tls_handle handle;
+					rats_tls_err_t err = rats_tls_init(&conf, &handle);
+					if (err != RATS_TLS_ERR_NONE) {
+						continue;
+					}
+
+					err = rats_tls_set_verification_callback(&handle,
+										 user_callback);
+					if (err != RATS_TLS_ERR_NONE) {
+						RTLS_ERR(
+							"Failed to set verification callback %#x\n",
+							err);
+						continue;
+					}
+					RTLS_INFO("Before negotiate \n");
+					err = rats_tls_negotiate(handle, sockfd);
+					if (err != RATS_TLS_ERR_NONE) {
+						rats_tls_cleanup(handle);
+						continue;
+					}
+
+					const char *msg = "ajh#jldsXoidjxdsa\n";
+
+					if (fuzzed_data.remaining_bytes() < 0) {
+						return 0;
+					}
+					RTLS_INFO("After nego, start transmit \n");
+
+					size_t len = strlen(msg);
+					err = rats_tls_transmit(handle, (void *)msg, &len);
+					if (err != RATS_TLS_ERR_NONE || len != strlen(msg)) {
+						RTLS_ERR("Failed to transmit %#x\n", err);
+						continue;
+					}
+
+					char buf[256];
+					len = sizeof(buf);
+					err = rats_tls_receive(handle, buf, &len);
+					if (err != RATS_TLS_ERR_NONE) {
+						continue;
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
